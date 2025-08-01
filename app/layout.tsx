89a2cd42627076d1bc3dc26395f6d931b6ff94b1
kayaliwanag <hello.icanif@gmail.com>
2025-08-01 08:59:29 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,14 @@ export const metadata = {
   description: "A daily journaling assistant by Kaya Liwanag.",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
   );
 }
-
